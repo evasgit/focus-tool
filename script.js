43710987064963534e3461f52268eba59fbca546
evasgit <39563095+evasgit@users.noreply.github.com>
2024-10-29 11:38:05 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let elapsedInterval;
 let player;
 let currentPlaylist = "";
 
-const versionNumber = "v1.0.3"; // 或從其他來源動態獲取版本號
+const versionNumber = "v1.0.4"; // 或從其他來源動態獲取版本號
 document.addEventListener("DOMContentLoaded", () => {
     const versionElement = document.getElementById("version");
     if (versionElement) {
@@ -114,11 +114,12 @@ const Timer = {
     },
 
     endBreak() {
-        notificationSound.play().catch(() => {
-            alert("音效播放失敗，可能受到瀏覽器限制");
-        });
         UI.resetBackground();
         UI.toggleTodoList(false);
+        notificationSound.play().catch(() => {
+            alert("休息結束音效播放失敗，可能受到瀏覽器限制");
+        });
+        History.updateHistoryDisplay(); // 更新歷史清單
     },
 
     updateGoal() {
