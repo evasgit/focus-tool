da48935d0e933f0d3edc3460aa48103240057adf
evasgit <39563095+evasgit@users.noreply.github.com>
2024-11-13 10:45:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let elapsedInterval;
 let player;
 let currentPlaylist = "";
 
-const versionNumber = "v1.0.17"; // 或從其他來源動態獲取版本號
+const versionNumber = "v1.0.18"; // 或從其他來源動態獲取版本號
 document.addEventListener("DOMContentLoaded", () => {
     const versionElement = document.getElementById("version");
     if (versionElement) {
