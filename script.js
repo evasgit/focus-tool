c9047207cbea52afa75a315fedcfc9b61e6ad92a
Eva <eva.yh.chiu@mail.foxconn.com>
2025-05-08 12:02:48 +0800

    v250508120236: 優化自訂時間的輸入筐

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let elapsedInterval;
 let player;
 let currentPlaylist = "";
 
-const versionNumber = "v250508115812";
+const versionNumber = "v250508120236";
 const DEBUG_MODE = false;
 
 document.addEventListener("DOMContentLoaded", () => {
