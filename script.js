0d2dc078b7956d28bacc205e81445c23c0b24e56
Eva <eva.yh.chiu@mail.foxconn.com>
2025-05-22 13:23:34 +0800

    v250522132327: 縮減寬度

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPlaylist = "";
 let notificationSound = new Audio("data/notification.mp3");
 let isRinging = false;
 
-const versionNumber = "v250522131949";
+const versionNumber = "v250522132327";
 const DEBUG_MODE = false;
 
 const TIMER_SETTINGS = {
