136f637d0d461c5e8839d0fb27e049002822b83e
Eva <eva.yh.chiu@mail.foxconn.com>
2025-05-22 13:03:50 +0800

    v250522130338: 20分鐘按鈕樣式

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPlaylist = "";
 let notificationSound = new Audio("data/notification.mp3");
 let isRinging = false;
 
-const versionNumber = "v250522130137";
+const versionNumber = "v250522130338";
 const DEBUG_MODE = false;
 
 const TIMER_SETTINGS = {
