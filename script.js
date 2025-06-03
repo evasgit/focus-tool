9e74056ca91d91ec1a6093c40423518a5271b825
Eva <eva.yh.chiu@mail.foxconn.com>
2025-06-03 13:44:05 +0800

    v250603134346: 加上github連結

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPlaylist = "";
 let notificationSound = new Audio("data/notification.mp3");
 let isRinging = false;
 
-const versionNumber = "v250528134619";
+const versionNumber = "v250603134346";
 const DEBUG_MODE = false;
 
 const TIMER_SETTINGS = {
