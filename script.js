76741d60e7b2b864699b2beb818d858606a41ee4
Eva <eva.yh.chiu@mail.foxconn.com>
2025-06-13 09:08:27 +0800

    v250613090753: 嘗試改善version被timer-display-section遮被的問題

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPlaylist = "";
 let notificationSound = new Audio("data/notification.mp3");
 let isRinging = false;
 
-const versionNumber = "v250613090545";
+const versionNumber = "v250613090753";
 const DEBUG_MODE = false;
 
 const TIMER_SETTINGS = {
