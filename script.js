88a40e283fd5401d6f73c77ee04eddd61f8f4764
Eva <eva.yh.chiu@mail.foxconn.com>
2025-08-05 10:03:24 +0800

    v250805100319: update

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let currentPlaylist = "";
 let notificationSound = new Audio("data/notification.mp3");
 let isRinging = false;
 
-const versionNumber = "v250805100309";
+const versionNumber = "v250805100319";
 const DEBUG_MODE = false;
 
 const TIMER_SETTINGS = {
